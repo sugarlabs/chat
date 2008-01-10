436ac55a7818d683c50245dfbb1b548677c27cb0
Morgan Collett <morgan.collett@gmail.com>
2008-01-10 16:57:32 +0200

    Copyright OLPC

--- pippy_app.py
+++ pippy_app.py
@@ -1,4 +1,4 @@
-# Copyright 2007 Collabora Ltd.
+# Copyright 2007-2008 One Laptop Per Child
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
