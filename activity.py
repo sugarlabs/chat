c0c0ce1de82d48c0471eee37e6273b2eb5eb8f40
James Cameron <quozl@laptop.org>
2017-05-04 13:15:13 +1000

    Fix version warnings from Python GI API

--- activity.py
+++ activity.py
@@ -17,12 +17,16 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
+import gi
+gi.require_version('Gtk', '3.0')
+
 from gi.repository import Gtk
 from gi.repository import Gdk
 from gi.repository import GdkPixbuf
 from gi.repository import GObject
 
 try:
+    gi.require_version('Gst', '1.0')
     from gi.repository import Gst
     _HAS_SOUND = True
 except:
