sugarlabs/chat

commit 613ea700bfd353b0ae6e2b02b5902de530bb5d38

Port Telepathy static binding to TelepathyGLib

activity.py
@@ -19,10 +19,12 @@
 
 import gi
 gi.require_version('Gtk', '3.0')
+gi.require_version('TelepathyGLib', '0.12')
 
 from gi.repository import Gtk
 from gi.repository import Gdk
 from gi.repository import GdkPixbuf
+from gi.repository import TelepathyGLib
 from gi.repository import GLib
 
 try:
@@ -40,15 +42,6 @@ import json
 import os
 import time
 from gettext import gettext as _
-
-from telepathy.interfaces import CHANNEL_INTERFACE
-from telepathy.interfaces import CHANNEL_INTERFACE_GROUP
-from telepathy.interfaces import CHANNEL_TYPE_TEXT
-from telepathy.interfaces import CONN_INTERFACE_ALIASING
-from telepathy.constants import CHANNEL_GROUP_FLAG_CHANNEL_SPECIFIC_HANDLES
-from telepathy.constants import CHANNEL_TEXT_MESSAGE_TYPE_NORMAL
-from telepathy.client import Connection
-from telepathy.client import Channel
 
 from sugar3.graphics import style
 from sugar3.graphics.icon import EventIcon, Icon
@@ -291,13 +284,12 @@ class Chat(activity.Activity):
             return
         bus_name, connection, channel = json.loads(tp_channel)
         logger.debug('GOT XMPP: %s %s %s', bus_name, connection, channel)
-        Connection(bus_name, connection, ready_handler=lambda conn:
-                   self._one_to_one_connection_ready_cb(
-                       bus_name, channel, conn))
+        conn = TelepathyGLib.Connection.new(bus_name, connection)
+        self._one_to_one_connection_ready_cb(TelepathyGLib.DBusDaemon.dup(), bus_name, channel, conn)
 
     def _one_to_one_connection_ready_cb(self, bus_name, channel, conn):
         '''Callback for Connection for one to one connection'''
-        text_channel = Channel(bus_name, channel)
+        text_channel = TelepathyGLib.Channel(conn, channel)
         self.text_channel = TextChannelWrapper(text_channel, conn)
         self.text_channel.set_received_callback(self._received_cb)
         self.text_channel.handle_pending_messages()
@@ -672,7 +664,7 @@ class TextChannelWrapper(object):
         self._logger = logging.getLogger(
             'chat-activity.TextChannelWrapper')
         self._signal_matches = []
-        m = self._text_chan[CHANNEL_INTERFACE].connect_to_signal(
+        m = self._text_chan[TelepathyGLib.IFACE_CHANNEL].connect_to_signal(
             'Closed', self._closed_cb)
         self._signal_matches.append(m)
 
@@ -684,14 +676,14 @@ class TextChannelWrapper(object):
         text = text.replace('/', SLASH)
 
         if self._text_chan is not None:
-            self._text_chan[CHANNEL_TYPE_TEXT].Send(
-                CHANNEL_TEXT_MESSAGE_TYPE_NORMAL, text)
+            self._text_chan[TelepathyGLib.IFACE_CHANNEL_TYPE_TEXT].Send(
+                TelepathyGLib.ChannelTextMessageType.NORMAL, text)
 
     def close(self):
         '''Close the text channel.'''
         self._logger.debug('Closing text channel')
         try:
-            self._text_chan[CHANNEL_INTERFACE].Close()
+            self._text_chan[TelepathyGLib.IFACE_CHANNEL].Close()
         except Exception:
             self._logger.debug('Channel disappeared!')
             self._closed_cb()
@@ -714,7 +706,7 @@ class TextChannelWrapper(object):
         if self._text_chan is None:
             return
         self._activity_cb = callback
-        m = self._text_chan[CHANNEL_TYPE_TEXT].connect_to_signal(
+        m = self._text_chan[TelepathyGLib.IFACE_CHANNEL_TYPE_TEXT].connect_to_signal(
             'Received', self._received_cb)
         self._signal_matches.append(m)
 
@@ -722,7 +714,7 @@ class TextChannelWrapper(object):
         '''Get pending messages and show them as received.'''
         for identity, timestamp, sender, type_, flags, text in \
             self._text_chan[
-                CHANNEL_TYPE_TEXT].ListPendingMessages(False):
+                TelepathyGLib.IFACE_CHANNEL_TYPE_TEXT].ListPendingMessages(False):
             self._received_cb(identity, timestamp, sender, type_, flags, text)
 
     def _received_cb(self, identity, timestamp, sender, type_, flags, text):
@@ -740,11 +732,11 @@ class TextChannelWrapper(object):
 
         if self._activity_cb:
             try:
-                self._text_chan[CHANNEL_INTERFACE_GROUP]
+                self._text_chan[TelepathyGLib.IFACE_CHANNEL_INTERFACE_GROUP]
             except Exception:
                 # One to one XMPP chat
                 nick = self._conn[
-                    CONN_INTERFACE_ALIASING].RequestAliases([sender])[0]
+                    TelepathyGLib.IFACE_CONNECTION_INTERFACE_ALIASING].RequestAliases([sender])[0]
                 buddy = {'nick': nick, 'color': '#000000,#808080'}
             else:
                 # Normal sugar3 MUC chat
@@ -752,7 +744,7 @@ class TextChannelWrapper(object):
                 buddy = self._get_buddy(sender)
             self._activity_cb(buddy, text)
             self._text_chan[
-                CHANNEL_TYPE_TEXT].AcknowledgePendingMessages([identity])
+                TelepathyGLib.IFACE_CHANNEL_TYPE_TEXT].AcknowledgePendingMessages([identity])
         else:
             self._logger.debug('Throwing received message on the floor'
                                ' since there is no callback connected. See'
@@ -774,13 +766,13 @@ class TextChannelWrapper(object):
         pservice = presenceservice.get_instance()
         # Get the Telepathy Connection
         tp_name, tp_path = pservice.get_preferred_connection()
-        conn = Connection(tp_name, tp_path)
-        group = self._text_chan[CHANNEL_INTERFACE_GROUP]
+        conn = TelepathyGLib.Connection.new(TelepathyGLib.DBusDaemon.dup(), tp_name, tp_path)
+        group = self._text_chan[TelepathyGLib.IFACE_CHANNEL_INTERFACE_GROUP]
         my_csh = group.GetSelfHandle()
         if my_csh == cs_handle:
             handle = conn.GetSelfHandle()
         elif group.GetGroupFlags() & \
-                CHANNEL_GROUP_FLAG_CHANNEL_SPECIFIC_HANDLES:
+                TelepathyGLib.ChannelGroupFlags.CHANNEL_SPECIFIC_HANDLES:
 
             handle = group.GetHandleOwners([cs_handle])[0]
         else:

commit b1290273e265e4b49a2d3ab887b7b63ab51dc4be

Add missing dbus daemon
Regression in 613ea70 discussed in https://github.com/sugarlabs/chat/pull/11

activity.py
@@ -284,8 +284,10 @@ class Chat(activity.Activity):
             return
         bus_name, connection, channel = json.loads(tp_channel)
         logger.debug('GOT XMPP: %s %s %s', bus_name, connection, channel)
-        conn = TelepathyGLib.Connection.new(bus_name, connection)
-        self._one_to_one_connection_ready_cb(TelepathyGLib.DBusDaemon.dup(), bus_name, channel, conn)
+        conn = TelepathyGLib.Connection.new(
+            TelepathyGLib.DBusDaemon.dup(), bus_name, connection)
+        self._one_to_one_connection_ready_cb(
+            TelepathyGLib.DBusDaemon.dup(), bus_name, channel, conn)
 
     def _one_to_one_connection_ready_cb(self, bus_name, channel, conn):
         '''Callback for Connection for one to one connection'''