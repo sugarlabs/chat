67e30d9a47f187b37d2c37eb508842659fff71d5
James Cameron <quozl@laptop.org>
2019-02-13 07:30:59 +1100

    flake8 fixes

--- chat/smilies.py
+++ chat/smilies.py
@@ -23,119 +23,120 @@ from gi.repository import GdkPixbuf
 from sugar3.graphics import style
 from sugar3.activity.activity import get_bundle_path
 
-THEME = [
-    # TRANS: A smiley (http://en.wikipedia.org/wiki/Smiley) explanation
-    # TRANS: ASCII-art equivalents are :-) and :)
-    ('smile', _('Smile'), [':-)', ':)']),
-    # TRANS: A smiley (http://en.wikipedia.org/wiki/Smiley) explanation
-    # TRANS: ASCII-art equivalents are ;-) and ;)
-    ('wink', _('Winking'), [';-)', ';)']),
-    # TRANS: A smiley (http://en.wikipedia.org/wiki/Smiley) explanation
-    # TRANS: ASCII-art equivalents are :-/ and :/
-    ('confused', _('Confused'), [':-/', ':/']),
-    # TRANS: A smiley (http://en.wikipedia.org/wiki/Smiley) explanation
-    # TRANS: ASCII-art equivalents are :-( and :(
-    ('sad', _('Sad'), [':-(', ':(']),
-    # TRANS: A smiley (http://en.wikipedia.org/wiki/Smiley) explanation
-    # TRANS: ASCII-art equivalents are :-D and :D
-    ('grin', _('Grin'), [':-D', ':D']),
-    # TRANS: A smiley (http://en.wikipedia.org/wiki/Smiley) explanation
-    # TRANS: ASCII-art equivalents are :-| and :|
-    ('neutral', _('Neutral'), (':-|', ':|')),
-    # TRANS: A smiley (http://en.wikipedia.org/wiki/Smiley) explanation
-    # TRANS: ASCII-art equivalents are :-O, :O, =-O and =O
-    ('shock', _('Shock'), [':-O', ':O', '=-O', '=O']),
-    # TRANS: A smiley (http://en.wikipedia.org/wiki/Smiley) explanation
-    # TRANS: ASCII-art equivalents are B-), B), 8-) and 8)
-    ('cool', _('Cool'), ['B-)', 'B)', '8-)', '8)']),
-    # TRANS: A smiley (http://en.wikipedia.org/wiki/Smiley) explanation
-    # TRANS: ASCII-art equivalents are :-P and :P
-    ('tongue', _('Tongue'), [':-P', ':P']),
-    # TRANS: A smiley (http://en.wikipedia.org/wiki/Smiley) explanation
-    # TRANS: ASCII-art equivalents are :">
-    ('blush', _('Blushing'), [':">']),
-    # TRANS: A smiley (http://en.wikipedia.org/wiki/Smiley) explanation
-    # TRANS: ASCII-art equivalents are :'-( and :'(
-    ('weep', _('Weeping'), [":'-(", ":'("]),
-    # TRANS: A smiley (http://en.wikipedia.org/wiki/Smiley) explanation
-    # TRANS: ASCII-art equivalents are O-), O), O:-) and O:)
-    ('angel', _('Angel'), ['O-)', 'O)', 'O:-)', 'O:)']),
-    # TRANS: A smiley (http://en.wikipedia.org/wiki/Smiley) explanation
-    # TRANS: ASCII-art equivalents are :-$ and :-$
-    ('shutup', _("Don't tell anyone"), (':-$', ':-$')),
-    # TRANS: A smiley (http://en.wikipedia.org/wiki/Smiley) explanation
-    # TRANS: ASCII-art equivalents are x-(, x(, X-( and x-(
-    ('angry', _('Angry'), ('x-(', 'x(', 'X-(', 'x-(')),
-    # TRANS: A smiley (http://en.wikipedia.org/wiki/Smiley) explanation
-    # TRANS: ASCII-art equivalents are >:> and >:)
-    ('devil', _('Devil'), ('>:>', '>:)')),
-    # TRANS: A smiley (http://en.wikipedia.org/wiki/Smiley) explanation
-    # TRANS: ASCII-art equivalents are :-B, :B
-    ('nerd', _('Nerd'), (':-B', ':B')),
-    # TRANS: A smiley (http://en.wikipedia.org/wiki/Smiley) explanation
-    # TRANS: ASCII-art equivalents are :-*, :*
-    ('kiss', _('Kiss'), (':-*', ':*')),
-    # TRANS: A smiley (http://en.wikipedia.org/wiki/Smiley) explanation
-    # TRANS: ASCII-art equivalents are :))
-    ('laugh', _('Laughing'), [':))']),
-    # TRANS: A smiley (http://en.wikipedia.org/wiki/Smiley) explanation
-    # TRANS: ASCII-art equivalents are I-)
-    ('sleep', _('Sleepy'), ['I-)']),
-    # TRANS: A smiley (http://en.wikipedia.org/wiki/Smiley) explanation
-    # TRANS: ASCII-art equivalents are :-&
-    ('sick', _('Sick'), [':-&']),
-    # TRANS: A smiley (http://en.wikipedia.org/wiki/Smiley) explanation
-    # TRANS: ASCII-art equivalents are /:)
-    ('eyebrow', _('Raised eyebrows'), ['/:)']),
-    ('unicode-1', _('Heart'), ['♥']),
-    ('unicode-2', _('Airplane'), ['✈']),
-    ('unicode-3', _('Music'), ['♬']),
-    ('unicode-4', _('Check box'), ['☑']),
-    ('unicode-5', _('Spade'), ['♠']),
-    ('unicode-6', _('Telephone'), ['☎']),
-    ('unicode-7', _('X box'), ['☒']),
-    ('unicode-8', _('Cadusis'), ['☤']),
-    ('unicode-9', _('Female'), ['♀']),
-    ('unicode-10', _('Star'), ['✩']),
-    ('unicode-11', _('Letter'), ['✉']),
-    ('unicode-12', _('Poison'), ['☠']),
-    ('unicode-13', _('Check mark'), ['✔']),
-    ('unicode-14', _('Male'), ['♂']),
-    ('unicode-15', _('Star'), ['★']),
-    ('unicode-16', _('Shamrock'), ['☘']),
-    ('unicode-17', _('Recycle'), ['♺']),
-    ('unicode-18', _('X'), ['✖']),
-    ('unicode-19', _('Fire'), ['♨']),
-    ('unicode-20', _('Leaf'), ['❦']),
-    ('unicode-21', _('Cloud'), ['☁']),
-    ('unicode-22', _('V'), ['✌']),
-    ('unicode-23', _('Crown'), ['♛']),
-    ('unicode-24', _('Floret'), ['❁']),
-    ('unicode-25', _('Star and crescent'), ['☪']),
-    ('unicode-26', _('Umbrella'), ['☂']),
-    ('unicode-27', _('Pencil'), ['✏']),
-    ('unicode-28', _('Floret'), ['❀']),
-    ('unicode-29', _('Snowman'), ['☃']),
-    ('unicode-30', _('Point right'), ['☛']),
-    ('unicode-31', _('Horse'), ['♞']),
-    ('unicode-32', _('Floret'), ['✿']),
-    ('unicode-33', _('Peace sign'), ['☮']),
-    ('unicode-34', _('Sun'), ['☼']),
-    ('unicode-35', _('Point left'), ['☚']),
-    ('unicode-36', _('Floret'), ['✾']),
-    ('unicode-37', _('Yin yang'), ['☯']),
-    ('unicode-38', _('Moon'), ['☾']),
-    ('unicode-39', _('Point up'), ['☝']),
-    ('unicode-40', _('Floret'), ['✽']),
-    ('unicode-41', _('Meteor'), ['☄']),
-    ('unicode-42', _('Point down'), ['☟']),
-    ('unicode-43', _('Floret'), ['✺']),
-    ('unicode-44', _('Scissors'), ['✂']),
-    ('unicode-45', _('Pen'), ['✍']),
-    ('unicode-46', _('Floret'), ['✵']),
-    ('unicode-47', _('Stars'), ['⁂']),
-    ('unicode-48', _('Place'), ['⌘']),
-    ('unicode-49', _('High voltage'), ['⚡']),
+THEME = \
+    [
+        # TRANS: A smiley (http://en.wikipedia.org/wiki/Smiley) explanation
+        # TRANS: ASCII-art equivalents are :-) and :)
+        ('smile', _('Smile'), [':-)', ':)']),
+        # TRANS: A smiley (http://en.wikipedia.org/wiki/Smiley) explanation
+        # TRANS: ASCII-art equivalents are ;-) and ;)
+        ('wink', _('Winking'), [';-)', ';)']),
+        # TRANS: A smiley (http://en.wikipedia.org/wiki/Smiley) explanation
+        # TRANS: ASCII-art equivalents are :-/ and :/
+        ('confused', _('Confused'), [':-/', ':/']),
+        # TRANS: A smiley (http://en.wikipedia.org/wiki/Smiley) explanation
+        # TRANS: ASCII-art equivalents are :-( and :(
+        ('sad', _('Sad'), [':-(', ':(']),
+        # TRANS: A smiley (http://en.wikipedia.org/wiki/Smiley) explanation
+        # TRANS: ASCII-art equivalents are :-D and :D
+        ('grin', _('Grin'), [':-D', ':D']),
+        # TRANS: A smiley (http://en.wikipedia.org/wiki/Smiley) explanation
+        # TRANS: ASCII-art equivalents are :-| and :|
+        ('neutral', _('Neutral'), (':-|', ':|')),
+        # TRANS: A smiley (http://en.wikipedia.org/wiki/Smiley) explanation
+        # TRANS: ASCII-art equivalents are :-O, :O, =-O and =O
+        ('shock', _('Shock'), [':-O', ':O', '=-O', '=O']),
+        # TRANS: A smiley (http://en.wikipedia.org/wiki/Smiley) explanation
+        # TRANS: ASCII-art equivalents are B-), B), 8-) and 8)
+        ('cool', _('Cool'), ['B-)', 'B)', '8-)', '8)']),
+        # TRANS: A smiley (http://en.wikipedia.org/wiki/Smiley) explanation
+        # TRANS: ASCII-art equivalents are :-P and :P
+        ('tongue', _('Tongue'), [':-P', ':P']),
+        # TRANS: A smiley (http://en.wikipedia.org/wiki/Smiley) explanation
+        # TRANS: ASCII-art equivalents are :">
+        ('blush', _('Blushing'), [':">']),
+        # TRANS: A smiley (http://en.wikipedia.org/wiki/Smiley) explanation
+        # TRANS: ASCII-art equivalents are :'-( and :'(
+        ('weep', _('Weeping'), [":'-(", ":'("]),
+        # TRANS: A smiley (http://en.wikipedia.org/wiki/Smiley) explanation
+        # TRANS: ASCII-art equivalents are O-), O), O:-) and O:)
+        ('angel', _('Angel'), ['O-)', 'O)', 'O:-)', 'O:)']),
+        # TRANS: A smiley (http://en.wikipedia.org/wiki/Smiley) explanation
+        # TRANS: ASCII-art equivalents are :-$ and :-$
+        ('shutup', _("Don't tell anyone"), (':-$', ':-$')),
+        # TRANS: A smiley (http://en.wikipedia.org/wiki/Smiley) explanation
+        # TRANS: ASCII-art equivalents are x-(, x(, X-( and x-(
+        ('angry', _('Angry'), ('x-(', 'x(', 'X-(', 'x-(')),
+        # TRANS: A smiley (http://en.wikipedia.org/wiki/Smiley) explanation
+        # TRANS: ASCII-art equivalents are >:> and >:)
+        ('devil', _('Devil'), ('>:>', '>:)')),
+        # TRANS: A smiley (http://en.wikipedia.org/wiki/Smiley) explanation
+        # TRANS: ASCII-art equivalents are :-B, :B
+        ('nerd', _('Nerd'), (':-B', ':B')),
+        # TRANS: A smiley (http://en.wikipedia.org/wiki/Smiley) explanation
+        # TRANS: ASCII-art equivalents are :-*, :*
+        ('kiss', _('Kiss'), (':-*', ':*')),
+        # TRANS: A smiley (http://en.wikipedia.org/wiki/Smiley) explanation
+        # TRANS: ASCII-art equivalents are :))
+        ('laugh', _('Laughing'), [':))']),
+        # TRANS: A smiley (http://en.wikipedia.org/wiki/Smiley) explanation
+        # TRANS: ASCII-art equivalents are I-)
+        ('sleep', _('Sleepy'), ['I-)']),
+        # TRANS: A smiley (http://en.wikipedia.org/wiki/Smiley) explanation
+        # TRANS: ASCII-art equivalents are :-&
+        ('sick', _('Sick'), [':-&']),
+        # TRANS: A smiley (http://en.wikipedia.org/wiki/Smiley) explanation
+        # TRANS: ASCII-art equivalents are /:)
+        ('eyebrow', _('Raised eyebrows'), ['/:)']),
+        ('unicode-1', _('Heart'), ['♥']),
+        ('unicode-2', _('Airplane'), ['✈']),
+        ('unicode-3', _('Music'), ['♬']),
+        ('unicode-4', _('Check box'), ['☑']),
+        ('unicode-5', _('Spade'), ['♠']),
+        ('unicode-6', _('Telephone'), ['☎']),
+        ('unicode-7', _('X box'), ['☒']),
+        ('unicode-8', _('Cadusis'), ['☤']),
+        ('unicode-9', _('Female'), ['♀']),
+        ('unicode-10', _('Star'), ['✩']),
+        ('unicode-11', _('Letter'), ['✉']),
+        ('unicode-12', _('Poison'), ['☠']),
+        ('unicode-13', _('Check mark'), ['✔']),
+        ('unicode-14', _('Male'), ['♂']),
+        ('unicode-15', _('Star'), ['★']),
+        ('unicode-16', _('Shamrock'), ['☘']),
+        ('unicode-17', _('Recycle'), ['♺']),
+        ('unicode-18', _('X'), ['✖']),
+        ('unicode-19', _('Fire'), ['♨']),
+        ('unicode-20', _('Leaf'), ['❦']),
+        ('unicode-21', _('Cloud'), ['☁']),
+        ('unicode-22', _('V'), ['✌']),
+        ('unicode-23', _('Crown'), ['♛']),
+        ('unicode-24', _('Floret'), ['❁']),
+        ('unicode-25', _('Star and crescent'), ['☪']),
+        ('unicode-26', _('Umbrella'), ['☂']),
+        ('unicode-27', _('Pencil'), ['✏']),
+        ('unicode-28', _('Floret'), ['❀']),
+        ('unicode-29', _('Snowman'), ['☃']),
+        ('unicode-30', _('Point right'), ['☛']),
+        ('unicode-31', _('Horse'), ['♞']),
+        ('unicode-32', _('Floret'), ['✿']),
+        ('unicode-33', _('Peace sign'), ['☮']),
+        ('unicode-34', _('Sun'), ['☼']),
+        ('unicode-35', _('Point left'), ['☚']),
+        ('unicode-36', _('Floret'), ['✾']),
+        ('unicode-37', _('Yin yang'), ['☯']),
+        ('unicode-38', _('Moon'), ['☾']),
+        ('unicode-39', _('Point up'), ['☝']),
+        ('unicode-40', _('Floret'), ['✽']),
+        ('unicode-41', _('Meteor'), ['☄']),
+        ('unicode-42', _('Point down'), ['☟']),
+        ('unicode-43', _('Floret'), ['✺']),
+        ('unicode-44', _('Scissors'), ['✂']),
+        ('unicode-45', _('Pen'), ['✍']),
+        ('unicode-46', _('Floret'), ['✵']),
+        ('unicode-47', _('Stars'), ['⁂']),
+        ('unicode-48', _('Place'), ['⌘']),
+        ('unicode-49', _('High voltage'), ['⚡']),
     ]
 
 SMILIES_SIZE = int(style.STANDARD_ICON_SIZE * 0.75)
@@ -200,7 +201,7 @@ def init():
                     fd = open(archivo, 'w')
                     fd.write(_generate_svg(codes[0]))
                     fd.close()
-                except IOError as e:
+                except IOError:
                     pass
         else:
             pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_size(
